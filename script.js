7cd84db3bde81a5dae3c67f9f800a186da4af8d2
Jacky <hoangj8@hotmail.com>
2021-01-29 20:41:56 -0500

    fixed typo in CSS which caused Nav bar to be unhidden on load and broke the toggle functionality.

---
+++ script.js
@@ -0,0 +1,21 @@
+var socialBar=document.querySelector("#socials");
+var contactButton=document.querySelector("#nav_contact");
+
+
+var socials_visible = false;
+
+
+function reveal(e){
+	e.preventDefault();
+	if(!socials_visible){
+		socialBar.setAttribute("style","transform: translateY(-200px); transition: transform 0.7s ease");
+		socials_visible=true;
+	}
+	else{
+		socials_visible=false;
+		socialBar.setAttribute("style","transform: translateY(+200px); transition: transform 0.7s ease-in");
+	}
+}
+
+
+contactButton.onclick = reveal;
